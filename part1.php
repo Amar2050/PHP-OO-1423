1de3578ae7ef33a284218b97d7159d3ca488db9a
Amar2050 <amarboutayeb@gmail.com>
2022-05-10 17:15:35 +0200

    Class Object Ballon V2

--- part1.php
+++ part1.php
@@ -36,26 +36,134 @@
 // ---------------- INSTANCIATION ------------
 
 
-// $ballonFoot = new stdClass();
-// $ballonFoot->type = "Football";
+// $b1 = new stdClass();
+// $b1->type = "Football";
 
-// var_dump($ballonFoot);
+// var_dump($b1);
 
 
 // ------------- SYNTAXE OBJECT OPERATOR -----------
 
-// $ballonFoot = new stdClass(); // j'instancie un objet a partir de la class STDCLASS
+$b1 = new stdClass(); // j'instancie un objet a partir de la class STDCLASS
 
-// $ballonFoot->type="Football"; // je lui assigne un attribut "type" qui prend la valeur "Football"
-// $ballonFoot->marque="Nike"; // je lui assigne une propriété "marque" qui prend la valeur "Nike"
-// $ballonFoot->taille= 5;     
-// $ballonFoot->status="crevé";
+$b1->type="Football"; // je lui assigne un attribut "type" qui prend la valeur "Football"
+$b1->marque="Nike"; // je lui assigne une propriété "marque" qui prend la valeur "Nike"
+$b1->taille= 5;     
+$b1->status="crevé";
 
 
-// // echo $ballonFoot; génére une erreur normal objet =! string
+// // echo $b1; génére une erreur normal objet =! string
 
-// // var_dump($ballonFoot); on a bien nos props !
+// // var_dump($b1); on a bien nos props !
 
-// echo " - Hey le ballon de $ballonFoot->type , $ballonFoot->marque est $ballonFoot->status Ismaël ! 
+// echo " - Hey le ballon de $b1->type , $b1->marque est $b1->status Ismaël ! 
 //         <br>
-//        - C'est un taille $ballonFoot->taille c'est ça Issac ?";
+//        - C'est un taille $b1->taille c'est ça Issac ?";
+
+
+//------------ Fonctions OBJETS
+
+
+// property_exists($obj , "maProps"); method_exists($obj , "maMethod") renvoie true ou false;
+
+
+// var_dump( property_exists($b1, "prix") );
+
+
+// MINI EXO avec property_exists() 
+
+// GOUDE  aka Les Goudes ! ou as Viking
+
+// if (!property_exists($b1,'prix')) {
+
+//     $b1->prix=5;
+//     echo "Le ballon coute $b1->prix";
+
+// }
+
+// ------------ PHP OO activated ----------------
+
+
+// var_dump( get_declared_classes() ); quelques class en OO ....
+
+// ---------------- Notre 1er CLASS
+
+
+// class Ballon {
+    
+//     public $marque;
+//     public $sport;
+
+//     function lancer(){
+//         echo "Vous avez lancé le ballon ! <br>";
+//     }
+
+// }
+
+// // On instancie 2 types de ballons
+
+
+// //----------Ballon de Foot -----------
+// $b1= new Ballon(); // Instanciation
+// $b1->marque = "Nike"; // on assigne à la props une valeur...
+// $b1->sport = "football";
+// // $b1->prix = 12; // ça marche ok intéret à voir 
+//                   //  plus tard
+
+// // var_dump($b1); // 
+// //----------Ballon de Rugby -----------
+// $ballonRugby= new Ballon(); // Instanciation
+// // $ballonRugby->marque = "Gilbert TM";
+// // $ballonRugby->sport = "rugby";
+// // var_dump($ballonRugby);
+// // ------- ACTION sur les objets
+
+// echo "Sport: $b1->sport  <br> Marque: $b1->marque";
+// echo "<br>";
+// $b1->lancer();
+
+
+// echo "<br><br><hr>";
+
+// echo "Sport: $ballonRugby->sport  <br> Marque: $ballonRugby->marque";
+// echo "<br>";
+// $ballonRugby->lancer();
+
+
+// -------------- V2 Ballon parametre à une méthode !
+
+
+class Ballon {
+    
+    public $marque;
+    public $sport;
+
+    function lancer($distance){
+        echo "Vous avez lancé le ballon à  $distance métres  ! <br>";
+    }
+
+}
+
+// On instancie 2 types de ballons
+
+
+//----------Ballon de Foot -----------
+$b1= new Ballon(); // Instanciation
+$b1->marque = "Nike"; // on assigne à la props une valeur...
+$b1->sport = "football";
+//----------Ballon de Rugby -----------
+$ballonRugby= new Ballon(); // Instanciation
+$ballonRugby->marque = "Gilbert TM";
+$ballonRugby->sport = "rugby";
+// ------- ACTION sur les objets
+
+echo "Sport: $b1->sport  <br> Marque: $b1->marque";
+echo "<br>";
+$b1->lancer(20);
+
+
+echo "<br><br><hr>";
+
+echo "Sport: $ballonRugby->sport  <br> Marque: $ballonRugby->marque";
+echo "<br>";
+$ballonRugby->lancer(70);
