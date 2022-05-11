6d9e5d69392cc53fa515fe43df9d3325bb2e3543
Amar2050 <amarboutayeb@gmail.com>
2022-05-11 13:12:13 +0200

    This Pseudo Variable

--- part1.php
+++ part1.php
@@ -172,24 +172,264 @@
 
 // ---------------- CLASS USER V0 ----------------
 
-class User {
-    public $name;
-    public $age;
+// class User {
+//     public $name;
+//     public $age;
 
-    function tchater($message){
-        return "<p> - $message</p>"; // on a remplacer le echo par return
+//     function tchater($message){
+//         echo "<p> - $message</p>"; // on a remplacer le echo par return
+//     }
+// }
+
+
+// $michel = new User();
+
+// $michel->name = "Michel";
+// $michel->age = 49;
+
+
+// $michel->tchater(" $michel->name : Salut salut !"); // ici un echo car la méthode return
+
+
+// class Impot{
+//     public $CA;
+  
+
+//     function calcule($CA){
+//         $x = $CA*0.2;
+//        echo "vous payerais $x € d'urssaf" ;
+//     }
+// }
+// $impot = new Impot();
+// $impot->calcule(1000);
+// // $impot->CA = 1000;
+
+
+class Charachters {
+    public $type;
+    public $ptLife;
+    public $specialPower;
+    public $specialDommage;
+
+    function attaquer($attaqueType, $dommage, $life) {
+        echo "l'attaque ". $attaqueType." est lancée et inflige ". $dommage ." dégats!! <br>";
+        $resteLife = $life - $dommage;
+        echo "il reste ". $resteLife  ." points de vie <br>";
     }
+
 }
 
+// $gandalf = new Charachters();
+// $gandalf->name="Gandalf";
+// $gandalf->type="Magicien";
+// $gandalf->ptLife=300;
+// $gandalf->specialPower="Poudre de perlinpinpin";
+// $gandalf->specialDommage= 250;
 
-$michel = new User();
+// $thor = new Charachters();
+// $thor->type="Guerrier";
+// $thor->name="Thor";
+// $thor->ptLife=500;
+// $thor->specialPower="Foudre impitoyable";
+// $thor->specialDommage= 150;
 
-$michel->name = "Michel";
-$michel->age = 49;
+// echo $gandalf->name. " attaque ". $thor->name ."<br>".$gandalf->attaquer($gandalf->specialPower, $gandalf->specialDommage, $thor->ptLife);
 
-$text = $michel->tchater(" $michel->name : Salut salut !");
 
-echo $michel->tchater(" $michel->name : Salut salut !"); // ici un echo car la méthode return
-var_dump($text); // on a le retour donc uniquement du string plus d'objet ici
-echo $text;    // on peut afficher / PDO et le fetchAll renvoi et convertit aussi
-echo htmlentities($text); // ici pour echapper le html donc ne pas interpreter
+// $macaron=  new user();
+
+// $macaron->name = "la duree";
+// $macaron->qualite = "excellent";
+
+// $text = $macaron->tchater("les macarons $macaron->name : sont tres bon");
+
+// echo $macaron->tchater("les macarons $macaron->name, sont tres bons");
+
+
+// class car {
+
+//     public $marque;
+//     public $proprietaire;
+//     public $status;
+
+//     function demarer($message){
+//         echo " $message ";
+//     }
+
+// };
+
+// $a = new car();
+
+// $a->marque = "ford";
+// $a->proprietaire= "Sam";
+
+// $a->demarer($a->proprietaire ." a demarer sa " .$a->marque);
+
+
+// class Personnage {
+//     public string $name;
+//     public int $hp;
+//     public int $mp;
+//     public string $classe;
+
+//     function construct($message){
+//         echo "<p> Salut ! Tu as bien fait de me choisir ! $message </p>";
+//     }
+// }
+
+// $legolas = new Personnage();
+
+// $legolas->name ="Legolas";
+// $legolas->hp = 20;
+// $legolas->mp = 12;
+// $legolas->classe = "archer";
+
+// $legolas->construct("Je suis $legolas->name, ton nouveau personnage, j'ai $legolas->hp HP et $legolas->mp MP,<br> je suis un $legolas->classe et donc je suis parfait pour faire des dégats de loin, allons dérouiller des Orcs et des Gobelins ! ");
+
+// $aragorn = new Personnage();
+
+// $aragorn->name = "Aragorn";
+// $aragorn->hp = 28;
+// $aragorn->mp = 10;
+// $aragorn->classe = "guerrier";
+
+// $aragorn->construct("Je suis $aragorn->name, je suis le meilleur $aragorn->classe de la Terre du milieu car je suis le Roi du Gondor (logique.), j'ai $aragorn->hp HP et $aragorn->mp MP donc tu peux compter sur moi pour faire une boucherie !");
+
+
+// class Parcours {
+//     public $temps;
+//     public $vitesse;
+
+//     function KmParcouru () {
+//         $x = $this->temps*$this->vitesse;
+//         echo "<p> - la distance parcourue est de $x </p>";
+//     }
+
+// }
+
+// $trajet1 = new Parcours();
+
+// $trajet1->temps = 2;
+// $trajet1->vitesse = 50;
+
+// $trajet1->KmParcouru($trajet1->temps,$trajet1->vitesse);
+
+// var_dump($trajet1);
+
+
+
+// class Viennoiserie {
+//     public $name;   
+//     public $chocolat;
+    
+//     function tQuoi($type) {
+//        if($type){
+//        echo"T'es une bonne chocolaTINE ! <br>";
+//         }else{
+//         echo"t'es un pain au chocolat ... beurk...";
+//         }
+//     }
+// }
+
+// $chocolatine = new Viennoiserie();
+// $chocolatine->name="chocolatine";
+// $chocolatine->chocolat=true;
+
+// $pain = new Viennoiserie();
+// $pain->name="pain";
+// $pain->chocolat=false;
+
+// $chocolatine->tQuoi($chocolatine->chocolat);
+// $pain->tQuoi($pain->chocolat);
+
+//  this POO 
+class Facture {
+
+    public $taux = 45;
+    public $client;
+    public $montant;
+
+
+    function afficherTaux(){
+        echo " <p>Taux: " . $this->taux ."%  </p>";
+    }
+
+    function afficherClient(){
+        echo "Client: $this->client  <br>";
+    }
+
+    function truc(){
+
+    }
+
+    function detailsObjet(){
+        var_dump($this); 
+    }
+
+} // ---> 
+
+// NIVEAU 1 --------> MANIPULER L'objet depuis l'intérieur de la class
+// NIVEAU 2 --------> $this EST l'objet !
+
+
+// NIVEAU 4 ---> 
+
+
+// PROFIL PSY !!!  PROBLEM SOLVING ! 
+
+
+// Lvl 2    | 
+
+// LVL 12   | 
+
+
+
+// -------------- WebForce3
+$fev= new Facture(); // Moule Facture
+$fev->client = "WebForce3";
+$fev->taux = 24;
+$fev->montant = 2500;
+$fev->afficherClient();
+$fev->afficherTaux();
+$fev->detailsObjet();
+
+var_dump($fev);
+
+// var_dump($fev);
+echo "<hr>";
+
+// ----------- Soros
+$mai = new Facture();
+$mai->client = "George Soros";
+$mai->montant = 450000;
+$mai->afficherTaux();
+$mai->afficherClient();
+$mai->detailsObjet();
+
+
+
+
+
+
+
+
+// // -------------- STAND By ---
+class Immobilier {
+
+    public $nom;
+    public $prix;
+    public $surface;
+
+    function calculPrix(){
+        $this->prix * $this->surface;
+    }
+
+
+}
+
+$bien1 = new Immobilier();
+
+// PDO 
+$apparts = $db->fetchAll();
+
+$bien->prix($apparts[0]['prix']);
