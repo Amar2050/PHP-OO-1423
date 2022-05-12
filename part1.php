b1d1c65908ff9a2b9500320a0e8bab655240e398
Amar2050 <amarboutayeb@gmail.com>
2022-05-12 16:44:33 +0200

    Exo Impro Database CREATE

--- part1.php
+++ part1.php
@@ -31,6 +31,27 @@ class Database {
 
     }
 
+    function create($nom,$possesseur,$console,$prix,$nbreJoueursMax,$commentaires){
+        $req = "INSERT INTO jeux_video (nom,possesseur,console,prix,nbre_joueurs_max,commentaires) VALUES         
+                         (:nom, 
+                         :possesseur,
+                         :console,
+                         :prix,
+                         :nbre_joueurs_max,
+                         :commentaires)";
+            
+             $stmt = $this->bdd->prepare($req);
+             $stmt->bindValue(":nom",$nom, PDO::PARAM_STR);
+             $stmt->bindValue(":possesseur",$possesseur, PDO::PARAM_STR);
+             $stmt->bindValue(":console",$console, PDO::PARAM_STR);
+             $stmt->bindValue(":prix",$prix, PDO::PARAM_INT);
+             $stmt->bindValue(":nbre_joueurs_max",$nbreJoueursMax, PDO::PARAM_INT);
+             $stmt->bindValue(":commentaires",$commentaires, PDO::PARAM_STR);
+             $stmt->execute();
+             $stmt->closeCursor();
+    }
+
+
 
 
 }
@@ -47,6 +68,11 @@ $db->fetchAll(); // envoi une req avec l'ensemble de data
 
 
 echo $db->myGames[5]["nom"]; // Par exemple !
+
+
+
+$db->create("Super Dinguerie 1423","Laura","Xbox",77,4,"Bonne capacité d'évolution malgré l'intro...4 jours à GAUCHE");
+
 
 // class Verre 
 // {
