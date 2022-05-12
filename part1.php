9a826b0921c9cd66f1192f2234ca02b01da9a349
Amar2050 <amarboutayeb@gmail.com>
2022-05-12 16:49:23 +0200

    Après Exo

--- part1.php
+++ part1.php
@@ -1,77 +1,42 @@
 <?php
 
+
 class Database {
 
-    public $bdd; // Online une fois new Database();
-    public $myGames; // full une fois $this->fetchAll()
+}
 
-    function __construct(){ 
-        $this->bdd = new PDO('mysql:host=localhost;dbname=jeux_projet', "root", "");
-    }
-
-    function statusBDD(){
-        var_dump($this->bdd->getAttribute(PDO::ATTR_CONNECTION_STATUS));
-    }
-
-    function fetchAll(){
-        $req = $this->bdd->prepare("SELECT * FROM jeux_video ");
-        $req->execute();
-        $this->myGames = $req->fetchAll(PDO::FETCH_ASSOC);
-        $req->closeCursor();
+function create($nom,$possesseur,$console,$prix,$nbreJoueursMax,$commentaires){
+    $req = "INSERT INTO jeux_video (nom,possesseur,console,prix,nbre_joueurs_max,commentaires) VALUES         
+                     (:nom, 
+                     :possesseur,
+                     :console,
+                     :prix,
+                     :nbre_joueurs_max,
+                     :commentaires)";
         
-    }
-
-    function read(){ 
-        $this->fetchAll(); // pour récupérer les datas quand on veut les lire !
-        echo "<ul>";
-        foreach($this->myGames as $game){
-            echo "<li>" .$game["nom"] . " </li>";
-        }
-        echo "</ul>";
-
-    }
-
-    function create($nom,$possesseur,$console,$prix,$nbreJoueursMax,$commentaires){
-        $req = "INSERT INTO jeux_video (nom,possesseur,console,prix,nbre_joueurs_max,commentaires) VALUES         
-                         (:nom, 
-                         :possesseur,
-                         :console,
-                         :prix,
-                         :nbre_joueurs_max,
-                         :commentaires)";
-            
-             $stmt = $this->bdd->prepare($req);
-             $stmt->bindValue(":nom",$nom, PDO::PARAM_STR);
-             $stmt->bindValue(":possesseur",$possesseur, PDO::PARAM_STR);
-             $stmt->bindValue(":console",$console, PDO::PARAM_STR);
-             $stmt->bindValue(":prix",$prix, PDO::PARAM_INT);
-             $stmt->bindValue(":nbre_joueurs_max",$nbreJoueursMax, PDO::PARAM_INT);
-             $stmt->bindValue(":commentaires",$commentaires, PDO::PARAM_STR);
-             $stmt->execute();
-             $stmt->closeCursor();
-    }
+         $stmt = $this->bdd->prepare($req);
+         $stmt->bindValue(":nom",$nom, PDO::PARAM_STR);
+         $stmt->bindValue(":possesseur",$possesseur, PDO::PARAM_STR);
+         $stmt->bindValue(":console",$console, PDO::PARAM_STR);
+         $stmt->bindValue(":prix",$prix, PDO::PARAM_INT);
+         $stmt->bindValue(":nbre_joueurs_max",$nbreJoueursMax, PDO::PARAM_INT);
+         $stmt->bindValue(":commentaires",$commentaires, PDO::PARAM_STR);
+         $stmt->execute();
+         $stmt->closeCursor();
+}
 
 
 
 
 }
-
-$db = new Database(); // grace à __construct connexion à la bdd
-
-// var_dump($db->myGames); // TEST 1 rien à ce moment normal !
-
-$db->statusBDD(); // comme on est connecté $bdd la props de l'objet $db contient notre Objet PDO et donc la connexion à la BDD 
-
 $db->fetchAll(); // envoi une req avec l'ensemble de data
-
-// var_dump($db->myGames); // TEST 2 maintenant on à charger la props $mygames
-
 
 echo $db->myGames[5]["nom"]; // Par exemple !
 
 
 
 $db->create("Super Dinguerie 1423","Laura","Xbox",77,4,"Bonne capacité d'évolution malgré l'intro...4 jours à GAUCHE");
+
 
 
 // class Verre 
